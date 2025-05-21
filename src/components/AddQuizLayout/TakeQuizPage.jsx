2287e5bf3d58a98a0835aee7d07206657bde8eaf
vigiperez24 <vigiperez2324@gmail.com>
2025-05-22 02:28:46 +0800

    Fixing TakeQuiz Page

--- src/components/AddQuizLayout/TakeQuizPage.jsx
+++ src/components/AddQuizLayout/TakeQuizPage.jsx
@@ -69,103 +69,131 @@ function TakeQuizPage() {
   ];
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [getStarted, setGetStarted] = useState(false);
+  let current = quizzes[currentQuestionIndex];
 
-  const currentQuiz = quizzes[currentQuestionIndex];
+  const handleNext = () => {
+    if (currentQuestionIndex < quizzes.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+      console.log(currentQuestionIndex);
+    }
+  };
 
   return (
     <div className="h-screen flex flex-col items-center justify-center ">
       <div className="max-w-md md:max-w-2xl space-y-4">
-        <div className="text-center">
-          <h1 className="text-xl">
-            Question: {currentQuestionIndex + 1} of {quizzes.length}
-          </h1>
-        </div>
         <div className=" bg-white rounded flex flex-col border-t-4 border-purple-700  ">
           {/* Start of Answerng */}
-          <div className="hidden">
-            <div className=" text-center text-2xl font-semibold">
-              <h1>Science</h1>
+          {!getStarted && (
+            <div className="flex flex-col gap-4 p-4 shadow-md">
+              <div className=" text-center text-2xl font-semibold">
+                <h1>Science</h1>
+              </div>
+              <hr />
+              <div className=" text-xl text-center text-gray-500">
+                <p>
+                  You will have {quizzes.length} questions. Answer them one by one. Best of
+                  Luck!
+                </p>
+              </div>
+              <div className=" text-center">
+                <button
+                  onClick={() => setGetStarted(true)}
+                  className="bg-buttonBg hover:bg-bgButtonHover p-2  text-lg text-white font-semibold rounded"
+                >
+                  Start Answering
+                </button>
+              </div>
             </div>
-            <hr />
-            <div className=" text-xl text-center text-gray-500">
-              <p>
-                You will have 10 questions. Answer them one by one. Best of
-                Luck!
-              </p>
-            </div>
-            <div className=" text-center">
-              <button className="bg-buttonBg hover:bg-bgButtonHover  text-lg text-white font-semibold rounded">
-                Start Answering
-              </button>
-            </div>
-          </div>
+          )}
 
           {/* Questions and Answer */}
 
-          <div className="flex flex-col  ">
-            <div className=" flex justify-between items-center bg-gray-200 text-gray-500 text-sm  py-3 px-4">
-              {/* Points and Secs */}
-              <div className="flex gap-4 items-center justify-between w-full ">
-                <p>1points</p>
-                <p className="text-xl text-gray-700 font-semibold">60s</p>
+          {getStarted && (
+            <div className="flex flex-col   shadow-md">
+              <div className=" flex justify-between items-center bg-gray-200 text-gray-500 text-sm  py-3 px-4 ">
+                {/* Points and Secs */}
+                <div className="flex gap-4 items-center justify-between w-full ">
+                  <p>1points</p>
+                  <p className="text-xl text-gray-700 font-semibold">60s</p>
+                </div>
               </div>
-            </div>
-            <hr />
-            {/* Questions */}
-            <div className="px-4 py-2">
-              <p className="text-base md:text-xl ">{currentQuiz.question}</p>
-            </div>
-            {/* Answer true or false*/}
-            <div className="px-4  py-2 flex gap-4 text-base md:text-xl  hidden">
-              <label className="flex  gap-1">
-                <input type="radio" name="" id="" />
-                True
-              </label>
-              <label className="flex  gap-1">
-                <input type="radio" name="" id="" />
-                False
-              </label>
-            </div>
+              <hr />
+              {/* Questions */}
+              <div className="px-4 py-2">
+                <p className="text-base md:text-xl ">{current.question}</p>
+              </div>
+              {/* Answer true or false*/}
+              {current.type === "True or False" && (
+                <div className="px-4  py-2 flex gap-4 text-base md:text-xl  ">
+                  <label className="flex  gap-1">
+                    <input type="radio" name="" id="" />
+                    True
+                  </label>
+                  <label className="flex  gap-1">
+                    <input type="radio" name="" id="" />
+                    False
+                  </label>
+                </div>
+              )}
+              {/* Identification */}
+              {current.type === "Identification" && (
+                <div className="px-4 mb-4 ">
+                  <input
+                    className="bg-gray-200 w-full p-2 rounded"
+                    placeholder="Enter correct answer"
+                    type="text"
+                  />
+                </div>
+              )}
 
-            {/* Identification */}
-            <div className="px-4 mb-4 hidden">
-              <input
-                className="bg-gray-200 w-full p-2 rounded"
-                placeholder="Enter correct answer"
-                type="text"
-              />
+              {/* Multiple Choices */}
+              {current.type === "Multiple Choices" && (
+                <div className="px-4 mb-4 space-y-2">
+                  <label className="flex gap-2 md:text-xl">
+                    <input type="radio" name="" id="" />
+                    A. Lorem ipsum dolor sit.
+                  </label>
+                  <label className="flex gap-2 md:text-xl">
+                    <input type="radio" name="" id="" />
+                    B. Lorem ipsum dolor sit.
+                  </label>
+                  <label className="flex gap-2 md:text-xl">
+                    <input type="radio" name="" id="" />
+                    C. Lorem ipsum dolor sit.
+                  </label>
+                  <label className="flex gap-2 md:text-xl">
+                    <input type="radio" name="" id="" />
+                    D. Lorem ipsum dolor sit.
+                  </label>
+                </div>
+              )}
             </div>
-
-            {/* Multiple Choices */}
-            <div className="px-4 mb-4 space-y-2">
-              <label className="flex gap-2 md:text-xl">
-                <input type="radio" name="" id="" />
-                A. Lorem ipsum dolor sit.
-              </label>
-              <label className="flex gap-2 md:text-xl">
-                <input type="radio" name="" id="" />
-                B. Lorem ipsum dolor sit.
-              </label>
-              <label className="flex gap-2 md:text-xl">
-                <input type="radio" name="" id="" />
-                C. Lorem ipsum dolor sit.
-              </label>
-              <label className="flex gap-2 md:text-xl">
-                <input type="radio" name="" id="" />
-                D. Lorem ipsum dolor sit.
-              </label>
-            </div>
-          </div>
+          )}
         </div>
         {/* Buttons */}
-        <div className="flex items-center justify-between ">
-          <button className="bg-gray-300 md:text-xl text-base p-2 rounded ">
-            Previous
-          </button>
-          <button className="bg-buttonBg text-white gray-300 md:text-xl text-base p-2 rounded">
-            Next
-          </button>
-        </div>
+        {getStarted && (
+          <div className="flex items-center justify-between ">
+            <button
+              onClick={handlePrevious}
+              className="bg-gray-300 md:text-xl text-base p-2 rounded "
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              className="bg-buttonBg text-white gray-300 md:text-xl text-base p-2 rounded"
+            >
+              Next
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
